151f2de0aaa7e710624cb873e00cdae4051f4070
BonSeung <bs.koo@sqisoft.com>
2024-07-17 17:37:41 +0900

    feat: EnableJpaAuditing 어노테이션 추가 - #19 - 자동으로 생성 시간, 수정 시간 넣어주는 기능

--- src/main/java/com/example/mutsideout_mju/MutsideoutMjuApplication.java
+++ src/main/java/com/example/mutsideout_mju/MutsideoutMjuApplication.java
@@ -2,8 +2,10 @@ package com.example.mutsideout_mju;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MutsideoutMjuApplication {
 
     public static void main(String[] args) {
