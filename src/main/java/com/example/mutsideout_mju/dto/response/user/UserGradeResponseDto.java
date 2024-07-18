7ee3c65ee88f081f20d160befebecc4f99f45317
BonSeung <bs.koo@sqisoft.com>
2024-07-19 03:13:56 +0900

    feat: 유저 등급 반환 DTO 추가 - #21

---
+++ src/main/java/com/example/mutsideout_mju/dto/response/user/UserGradeResponseDto.java
@@ -0,0 +1,20 @@
+package com.example.mutsideout_mju.dto.response.user;
+
+import com.example.mutsideout_mju.entity.Grade;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserGradeResponseDto {
+    private String name;
+    private Grade grade;
+
+    public static UserGradeResponseDto of(String name, Grade grade) {
+        return UserGradeResponseDto.builder()
+                .name(name)
+                .grade(grade)
+                .build();
+    }
+}
