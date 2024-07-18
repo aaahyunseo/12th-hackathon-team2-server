37e3664ec69ad99d086dbd6eae153fcb6bf67be9
aaahyunseo <hs030608@naver.com>
2024-07-19 00:01:34 +0900

    feat: Room Entity 수정 - #22

--- src/main/java/com/example/mutsideout_mju/entity/Room.java
+++ src/main/java/com/example/mutsideout_mju/entity/Room.java
@@ -23,4 +23,19 @@ public class Room extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    public Room setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public Room setLink(String link) {
+        this.link = link;
+        return this;
+    }
+
+    public Room setContent(String content) {
+        this.content = content;
+        return this;
+    }
 }
