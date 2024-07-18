4d945f71644fa7f9bccb05cb0de8346dcc0a402e
BonSeung <bs.koo@sqisoft.com>
2024-07-19 03:11:31 +0900

    feat: UserSurveyRepository 추가 - #21

---
+++ src/main/java/com/example/mutsideout_mju/repository/UserSurveyRepository.java
@@ -0,0 +1,11 @@
+package com.example.mutsideout_mju.repository;
+
+import com.example.mutsideout_mju.entity.UserSurvey;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface UserSurveyRepository extends JpaRepository<UserSurvey, UUID> {
+    List<UserSurvey> findByUserId(UUID userId);
+}
