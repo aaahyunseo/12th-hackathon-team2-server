c38d8488d2efda7c3b670dd3af337c928f1745d3
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro.local>
2024-07-18 20:40:28 +0900

    feature: dto 추가 - #6 GroupedCompletedPlannerResponse, PlannerListResponseData, CompletePlannerRequestDto 추가

---
+++ src/main/java/com/example/mutsideout_mju/dto/response/planner/GroupedCompletedPlannerResponse.java
@@ -0,0 +1,16 @@
+package com.example.mutsideout_mju.dto.response.planner;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class GroupedCompletedPlannerResponse {
+    private Map<LocalDate, List<CompletedPlannerResponse>> groupedPlanners;
+}
