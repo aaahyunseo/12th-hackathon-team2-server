ba726c2d88573cd0262c66a4111cc439a8dae49a
BonSeung <bs.koo@sqisoft.com>
2024-07-19 16:41:16 +0900

    fix: 로그인 검증 어노테이션 적용 URL 수정 - #25

--- src/main/java/com/example/mutsideout_mju/config/AuthenticationConfig.java
+++ src/main/java/com/example/mutsideout_mju/config/AuthenticationConfig.java
@@ -19,11 +19,8 @@ public class AuthenticationConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
         registry.addInterceptor(authenticationInterceptor)
-                .addPathPatterns("/planners/**")
-                .addPathPatterns("/diaries/**")
-                .addPathPatterns("/surveys/**")
-                .addPathPatterns("/rooms/**")
-                .excludePathPatterns("/auth/**");
+                .addPathPatterns("/planners/**", "/diaries/**", "/rooms/**", "/surveys/**", "/auth/logout")
+                .excludePathPatterns("/auth/signup", "/auth/login", "/questions");
     }
 
     @Override
