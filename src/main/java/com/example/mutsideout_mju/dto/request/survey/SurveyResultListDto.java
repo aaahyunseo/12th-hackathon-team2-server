a2e643b192be3b2de8117d8ab3daa350dbe9663d
BonSeung <bs.koo@sqisoft.com>
2024-07-19 14:57:40 +0900

    fix: 역직렬화를 위해 NoArgs 어노테이션 추가 -#21 - 컬렉션 타입엔 NotBlank 어노테이션이 적합하지 않아 NotEmpty 어노테이션으로 변경

--- src/main/java/com/example/mutsideout_mju/dto/request/survey/SurveyResultListDto.java
+++ src/main/java/com/example/mutsideout_mju/dto/request/survey/SurveyResultListDto.java
@@ -1,16 +1,15 @@
 package com.example.mutsideout_mju.dto.request.survey;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 public class SurveyResultListDto {
-    @NotBlank(message = "설문조사 답변이 누락되었습니다.")
+    @NotEmpty(message = "설문조사 답변이 누락되었습니다.")
     private List<SurveyResult> surveyResultList;
-
-    public SurveyResultListDto(List<SurveyResult> resultList) {
-        this.surveyResultList = resultList;
-    }
 }
