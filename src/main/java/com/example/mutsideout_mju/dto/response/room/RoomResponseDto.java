05b61846bd4dd697a8d9abb7899dfd6a875a4936
aaahyunseo <hs030608@naver.com>
2024-07-22 23:27:34 +0900

    feat: RoomResponseDto 활성화 필드 삭제 - #22

--- src/main/java/com/example/mutsideout_mju/dto/response/room/RoomResponseDto.java
+++ src/main/java/com/example/mutsideout_mju/dto/response/room/RoomResponseDto.java
@@ -14,15 +14,12 @@ public class RoomResponseDto {
     private UUID id;
     private String title;
     private String createdAt;
-    private boolean isActive;
 
     public static RoomResponseDto roomResponseDto(Room room){
         return RoomResponseDto.builder()
                 .id(room.getId())
                 .title(room.getTitle())
                 .createdAt(room.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
-                //현재 시각으로 부터 방 생성 시간이 24시간 이전이면 true(활성화), 24시간 이후이면 false(비활성화)
-                .isActive(room.getCreatedAt().isAfter(LocalDateTime.now().minusHours(24)))
                 .build();
     }
 }
