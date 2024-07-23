b00d3edd0c6af1dec6dc5474d06c81e8cb305ebd
BonSeung <bs.koo@sqisoft.com>
2024-07-23 17:56:56 +0900

    feat: 유저 등급 담아 반환하는 메서드 추가 - #45

--- src/main/java/com/example/mutsideout_mju/service/UserService.java
+++ src/main/java/com/example/mutsideout_mju/service/UserService.java
@@ -31,7 +31,11 @@ public class UserService {
         user.setUserGrade(grade);
         userRepository.save(user);
 
-        return UserGradeResponseDto.of(user.getName(), grade);
+        return UserGradeResponseDto.fromUserDetails(user.getName(), grade);
+    }
+
+    public UserGradeResponseDto getUserGrade(User user) {
+        return UserGradeResponseDto.fromGradeOnly(user.getUserGrade());
     }
 
     public static boolean isValidSurveyOption(UserSurvey userSurvey) {
