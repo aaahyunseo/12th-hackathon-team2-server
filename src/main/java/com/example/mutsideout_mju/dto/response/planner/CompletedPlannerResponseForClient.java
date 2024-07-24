31bd53c4e014555f40a02c8e2b3624ef7be0a885
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro.local>
2024-07-24 10:28:21 +0900

    feat: response 용 dto 생성 - #49

---
+++ src/main/java/com/example/mutsideout_mju/dto/response/planner/CompletedPlannerResponseForClient.java
@@ -0,0 +1,18 @@
+package com.example.mutsideout_mju.dto.response.planner;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class CompletedPlannerResponseForClient {
+    private final UUID plannerId;
+    private final String content;
+    private final boolean isCompleted;
+    private final String formattedDate;
+    private final UUID userId;
+}
