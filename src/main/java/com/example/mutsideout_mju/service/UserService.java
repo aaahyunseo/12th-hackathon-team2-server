e01bf528735ca09ba68b183e5c1885945fc3034e
aaahyunseo <hs030608@naver.com>
2024-07-27 20:45:45 +0900

    feat: UserService 회원 정보 수정 및 탈퇴 기능 추가 - #58

--- src/main/java/com/example/mutsideout_mju/service/UserService.java
+++ src/main/java/com/example/mutsideout_mju/service/UserService.java
@@ -1,10 +1,15 @@
 package com.example.mutsideout_mju.service;
 
+import com.example.mutsideout_mju.authentication.PasswordHashEncryption;
+import com.example.mutsideout_mju.dto.request.user.DeleteUserDto;
+import com.example.mutsideout_mju.dto.request.user.UpdateUserDto;
 import com.example.mutsideout_mju.dto.response.user.UserGradeResponseDto;
 import com.example.mutsideout_mju.entity.Grade;
 import com.example.mutsideout_mju.entity.SurveyOption;
 import com.example.mutsideout_mju.entity.User;
 import com.example.mutsideout_mju.entity.UserSurvey;
+import com.example.mutsideout_mju.exception.ForbiddenException;
+import com.example.mutsideout_mju.exception.errorCode.ErrorCode;
 import com.example.mutsideout_mju.repository.UserRepository;
 import com.example.mutsideout_mju.repository.UserSurveyRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +23,7 @@ import java.util.List;
 public class UserService {
     private final UserRepository userRepository;
     private final UserSurveyRepository userSurveyRepository;
+    private final PasswordHashEncryption passwordHashEncryption;
 
     @Transactional
     public UserGradeResponseDto calculateUserGrade(User user) {
@@ -44,5 +50,31 @@ public class UserService {
 
         return (questionNumber >= 1 && questionNumber <= 3 && (option == SurveyOption.NORMAL || option == SurveyOption.YES))
                 || (questionNumber >= 4 && questionNumber <= 6 && option == SurveyOption.YES);
+    }
+
+    //회원 탈퇴
+    public void deleteUser(User user, DeleteUserDto deleteUserDto) {
+        validatePassword(deleteUserDto.getPassword(), user.getPassword());
+        userRepository.delete(user);
+    }
+
+    //회원 정보 수정
+    @Transactional
+    public void updateUser(User user, UpdateUserDto updateUserDto) {
+        validatePassword(updateUserDto.getOriginPassword(), user.getPassword());
+        if (updateUserDto.getNewName() != null && !updateUserDto.getNewName().isEmpty()) {
+            user.setName(updateUserDto.getNewName());
+        }
+        if (updateUserDto.getNewPassword() != null && !updateUserDto.getNewPassword().isEmpty()) {
+            user.setPassword(passwordHashEncryption.encrypt(updateUserDto.getNewPassword()));
+        }
+        userRepository.save(user);
+    }
+
+    //비밀번호 일치 여부 확인
+    public void validatePassword(String plainPassword, String hashedPassword) {
+        if (!passwordHashEncryption.matches(plainPassword, hashedPassword)) {
+            throw new ForbiddenException(ErrorCode.NO_ACCESS, "비밀번호 정보가 일치하지 않습니다.");
+        }
     }
 }
