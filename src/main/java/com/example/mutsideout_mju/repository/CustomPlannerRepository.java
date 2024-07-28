47b4f1bd6b85b2cca6f537f637e5dc8bf5f51763
BonSeung <bs.koo@sqisoft.com>
2024-07-28 21:48:03 +0900

    refactor: 메서드 이름 변경 - #57

--- src/main/java/com/example/mutsideout_mju/repository/CustomPlannerRepository.java
+++ src/main/java/com/example/mutsideout_mju/repository/CustomPlannerRepository.java
@@ -5,5 +5,5 @@ import java.util.Map;
 import java.util.UUID;
 
 public interface CustomPlannerRepository {
-    Map<String, Long> getCompletedPlannersStatistics(UUID userId, LocalDate startDate, LocalDate endDate);
+    Map<String, Long> getCompletedPlannersStats(UUID userId, LocalDate startDate, LocalDate endDate);
 }
