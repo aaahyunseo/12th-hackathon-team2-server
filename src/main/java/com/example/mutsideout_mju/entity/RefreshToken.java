c5a98060c20b1a9a32fb0c96db700f3d91c99220
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro.local>
2024-07-29 21:49:07 +0900

    feat: RefreshToken 엔티티 수정 - #66

--- src/main/java/com/example/mutsideout_mju/entity/RefreshToken.java
+++ src/main/java/com/example/mutsideout_mju/entity/RefreshToken.java
@@ -1,17 +1,52 @@
 package com.example.mutsideout_mju.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "refresh_token")
 public class RefreshToken {
-    UUID userId;
+    @jakarta.persistence.Id
     @Id
-    UUID tokenId;
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "uuid2")
+    @Column(updatable = false, unique = true, nullable = false)
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID userId;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    public RefreshToken(UUID userId, String token) {
+        this.userId = userId;
+        this.token = token;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
 }
