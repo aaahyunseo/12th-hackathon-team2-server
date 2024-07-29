e4c5d7f44a62f356773592daa86757ec712bd1f8
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro.local>
2024-07-29 16:22:00 +0900

    feat: refreshToken - #66

--- src/main/java/com/example/mutsideout_mju/entity/RefreshToken.java
+++ src/main/java/com/example/mutsideout_mju/entity/RefreshToken.java
@@ -1,4 +1,17 @@
 package com.example.mutsideout_mju.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class RefreshToken {
+    UUID userId;
+    @Id
+    UUID tokenId;
 }
