fd4497c5c47eb8c1ef5421ea2df6fecfa5d3a22a
BonSeung <bs.koo@sqisoft.com>
2024-07-29 11:31:52 +0900

    fix: 브랜치 충돌 해결 - #57

--- src/main/java/com/example/mutsideout_mju/entity/User.java
+++ src/main/java/com/example/mutsideout_mju/entity/User.java
@@ -48,14 +48,6 @@ public class User extends BaseEntity {
         this.password = password;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public Grade determineGrade(long count) {
         if (count >= 4) {
             return Grade.TOMATO;
