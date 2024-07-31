eac794a9487d21da70a3b582823b687b5350646a
aaahyunseo <hs030608@naver.com>
2024-07-31 18:19:49 +0900

    feat: DiaryResponseData 다중 이미지 불러오기 - #74

--- src/main/java/com/example/mutsideout_mju/dto/response/diary/DiaryResponseData.java
+++ src/main/java/com/example/mutsideout_mju/dto/response/diary/DiaryResponseData.java
@@ -1,11 +1,14 @@
 package com.example.mutsideout_mju.dto.response.diary;
 
 import com.example.mutsideout_mju.entity.Diary;
+import com.example.mutsideout_mju.entity.ImageFile;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Getter
 @Builder
@@ -13,15 +16,19 @@ public class DiaryResponseData {
     private UUID id;
     private String title;
     private String content;
-    private String imageUrl;
+    private List<String> images;
     private String createdAt;
 
-    public static DiaryResponseData from(Diary diary){
+    public static DiaryResponseData from(Diary diary) {
+        List<String> images = diary.getImageFiles().stream()
+                .map(ImageFile::getImageUrl)
+                .collect(Collectors.toList());
+
         return DiaryResponseData.builder()
                 .id(diary.getId())
                 .title(diary.getTitle())
                 .content(diary.getContent())
-                .imageUrl(diary.getImageUrl())
+                .images(images)
                 .createdAt(diary.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
                 .build();
     }
