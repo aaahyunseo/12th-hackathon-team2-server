c7ff1377bec5c181ae66769509d3a61e8f6dd960
aaahyunseo <hs030608@naver.com>
2024-07-31 18:22:10 +0900

    feat: ImageFileService 추가 - #74

---
+++ src/main/java/com/example/mutsideout_mju/service/ImageFileService.java
@@ -0,0 +1,53 @@
+package com.example.mutsideout_mju.service;
+
+import com.example.mutsideout_mju.entity.Diary;
+import com.example.mutsideout_mju.entity.ImageFile;
+import com.example.mutsideout_mju.entity.User;
+import com.example.mutsideout_mju.repository.ImageFileRepository;
+import com.example.mutsideout_mju.util.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ImageFileService {
+    private final ImageFileRepository imageFileRepository;
+    private final S3Service s3Service;
+
+    /**
+     * 이미지 업로드
+     */
+    @Transactional
+    public void uploadImages(User user, Diary diary, List<MultipartFile> images) throws IOException {
+        for (MultipartFile img : images) {
+            if (img != null && !img.isEmpty()) {
+                String imageUrl = s3Service.uploadImage(img);
+                ImageFile newImage = ImageFile.builder()
+                        .imageUrl(imageUrl)
+                        .diary(diary)
+                        .user(user)
+                        .build();
+                imageFileRepository.save(newImage);
+            }
+        }
+    }
+
+    /**
+     * 이미지 삭제
+     */
+    @Transactional
+    public void deleteImages(Diary diary) {
+        if (diary.getImageFiles() != null) {
+            for (ImageFile img : diary.getImageFiles()) {
+                s3Service.deleteImage(img.getImageUrl());
+            }
+            diary.getImageFiles().clear();
+            imageFileRepository.deleteAll(diary.getImageFiles());
+        }
+    }
+}
