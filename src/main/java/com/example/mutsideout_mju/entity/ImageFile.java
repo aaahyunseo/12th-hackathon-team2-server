35466d450c44fa03bbcb519eea702b895d56657c
aaahyunseo <hs030608@naver.com>
2024-07-31 18:21:11 +0900

    feat: ImageFile 엔티티 생성 - #74

---
+++ src/main/java/com/example/mutsideout_mju/entity/ImageFile.java
@@ -0,0 +1,26 @@
+package com.example.mutsideout_mju.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "image_files")
+public class ImageFile extends BaseEntity {
+    @Column
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "diary_id")
+    private Diary diary;
+}
