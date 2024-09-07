3bc772a9414e9541c675f9de11f754d142e253af
aaahyunseo <hs030608@naver.com>
2024-09-07 17:52:12 +0900

    refactor: PaginationDto 페이지 사이즈 검증 - #87

--- src/main/java/com/example/mutsideout_mju/dto/request/PaginationDto.java
+++ src/main/java/com/example/mutsideout_mju/dto/request/PaginationDto.java
@@ -1,14 +1,18 @@
 package com.example.mutsideout_mju.dto.request;
 
+import com.example.mutsideout_mju.exception.NotFoundException;
+import com.example.mutsideout_mju.exception.errorCode.ErrorCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 public class PaginationDto {
-    private final int PAGE_SIZE = 10;    //10개씩 페이징
+    private final int pageSize = 10;    //10개씩 페이징
     private int page;                    //요청받은 페이지
 
     public PaginationDto(int page) {
+        if (pageSize <= page && page != 0) {
+            throw new NotFoundException(ErrorCode.NOT_FOUND_PAGE);
+        }
         this.page = Math.max(page - 1, 0);
     }
 }
