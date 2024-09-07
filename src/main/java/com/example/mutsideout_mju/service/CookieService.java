f0a967d007043826c315a2a5cd0aafcec6f60b6c
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro.local>
2024-09-07 17:43:14 +0900

    feat: CookieService 생성 - #88

---
+++ src/main/java/com/example/mutsideout_mju/service/CookieService.java
@@ -0,0 +1,13 @@
+package com.example.mutsideout_mju.service;
+
+import com.example.mutsideout_mju.authentication.AuthenticationExtractor;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+@Service
+public class CookieService {
+
+}
