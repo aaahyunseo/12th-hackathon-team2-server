67f0e500b7667b6356418696aa917390a4f4cc48
BonSeung <bs.koo@sqisoft.com>
2024-09-07 18:05:05 +0900

    feat: 유저 검증 메서드 boolean 반환으로 변경 - #90

--- src/main/java/com/example/mutsideout_mju/repository/UserRepository.java
+++ src/main/java/com/example/mutsideout_mju/repository/UserRepository.java
@@ -7,7 +7,10 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByName(String name);
 
     Optional<User> findByEmail(String email);
+
+    boolean existsByName(String name);
+
+    boolean existsByEmail(String email);
 }
