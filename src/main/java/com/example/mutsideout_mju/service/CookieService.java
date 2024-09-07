5e68d033f63eaa648a8d3a91a9ba3c154fca01f8
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro.local>
2024-09-07 17:46:03 +0900

    feature: CookieService로 token 생성 역할 분리 - #88

--- src/main/java/com/example/mutsideout_mju/service/CookieService.java
+++ src/main/java/com/example/mutsideout_mju/service/CookieService.java
@@ -9,5 +9,26 @@ import org.springframework.stereotype.Service;
 import java.time.Duration;
 @Service
 public class CookieService {
+    public void setCookie(HttpServletResponse response, String accessToken) {
+        ResponseCookie cookie = ResponseCookie.from(AuthenticationExtractor.TOKEN_COOKIE_NAME, accessToken)
+                .maxAge(Duration.ofMillis(1800000))
+                .path("/")
+                .httpOnly(true)
+                .sameSite("None").secure(true)
+                .build();
 
+        response.addHeader("set-cookie", cookie.toString());
+    }
+
+    public void setCookieForRefreshToken(HttpServletResponse response, String refreshToken) {
+        ResponseCookie cookie_refresh = ResponseCookie.from("RefreshToken", refreshToken)
+                .maxAge(Duration.ofDays(14))
+                .path("/")
+                .httpOnly(true)
+                .sameSite("None")
+                .secure(true)
+                .build();
+
+        response.addHeader("set-cookie", cookie_refresh.toString());
+    }
 }
