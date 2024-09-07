6bf744d3f8893ed2e758a166af04b2d7581eb0a4
백가현 <baeggahyeon@baeggahyeon-ui-MacBookPro.local>
2024-09-07 18:38:39 +0900

    feat: AccessRokenProvider class 이름 변경

--- src/main/java/com/example/mutsideout_mju/authentication/AuthenticationInterceptor.java
+++ src/main/java/com/example/mutsideout_mju/authentication/AuthenticationInterceptor.java
@@ -20,12 +20,12 @@ import java.util.UUID;
 public class AuthenticationInterceptor implements HandlerInterceptor {
     private final UserRepository userRepository;
     private final AuthenticationContext authenticationContext;
-    private final JwtTokenProvider jwtTokenProvider;
+    private final AccessTokenProvider accessTokenProvider;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws UnsupportedEncodingException {
         String accessToken = AuthenticationExtractor.extract(request);
-        UUID userId = UUID.fromString(jwtTokenProvider.getPayload(accessToken));
+        UUID userId = UUID.fromString(accessTokenProvider.getPayload(accessToken));
         User user = findExistingUser(userId);
         authenticationContext.setPrincipal(user);
         return true;
