e07362e1c034f4a0a2260a47471c7465b5603b07
BonSeung <bs.koo@sqisoft.com>
2024-09-09 11:23:03 +0900

    refactor: 인터셉터 적용 PATH 엔티티별로 분리 - #96

--- src/main/java/com/example/mutsideout_mju/config/AuthenticationConfig.java
+++ src/main/java/com/example/mutsideout_mju/config/AuthenticationConfig.java
@@ -13,14 +13,27 @@ import java.util.List;
 @Configuration
 @RequiredArgsConstructor
 public class AuthenticationConfig implements WebMvcConfigurer {
+
     private final AuthenticationInterceptor authenticationInterceptor;
     private final AuthenticatedUserArgumentResolver authenticatedUserArgumentResolver;
+    private static final String[] ADD_PLANNER_API_PATH = {"/planners", "planners/{plannerId}", "/planners/{plannerId}/complete", "planners/calendars"};
+    private static final String[] ADD_DIARY_API_PATH = {"/diaries", "diaries/{diaryId}"};
+    private static final String[] ADD_ROOM_API_PATH = {"/rooms", "rooms/{roomId}"};
+    private static final String[] ADD_USER_API_PATH = {"/users", "users/grade", "users/mypage"};
+    private static final String ADD_SURVEY_API_PATH = "/surveys";
+    private static final String ADD_AUTH_API_PATH = "/auth/logout";
+    private static final String[] EXCLUDE_AUTH_API_PATH = {"/auth/login", "/auth/signup", "/auth/refresh"};
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
         registry.addInterceptor(authenticationInterceptor)
-                .addPathPatterns("/planners/**", "/diaries/**", "/rooms/**", "/users/**", "/surveys", "/auth/logout")
-                .excludePathPatterns("/auth/signup", "/auth/login", "/auth/refresh");
+                .addPathPatterns(ADD_PLANNER_API_PATH)
+                .addPathPatterns(ADD_DIARY_API_PATH)
+                .addPathPatterns(ADD_ROOM_API_PATH)
+                .addPathPatterns(ADD_USER_API_PATH)
+                .addPathPatterns(ADD_SURVEY_API_PATH)
+                .addPathPatterns(ADD_AUTH_API_PATH)
+                .excludePathPatterns(EXCLUDE_AUTH_API_PATH);
     }
 
     @Override
